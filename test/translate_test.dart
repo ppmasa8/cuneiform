66b360a5422169514b4cfd9e2ed8f4ed3845ee8b
ppmasa8 <kiyokiyopip56@gmail.com>
2022-04-12 22:45:43 +0900

    Added new test cases

--- test/translate_test.dart
+++ test/translate_test.dart
@@ -8,4 +8,18 @@ void main() {
     var answer = Translate.Execute(letter);
     expect(answer, translatedChar);
   });
+
+  test('If it contains characters that cannot be converted', () {
+    var letter = 'a1cio22hstriovhaoierhg';
+    var translatedChar = '𒀀1𒋫𒄿𒃰22𒄩𒋢𒋼𒊒𒄿𒃰𒐊𒄩𒀀𒃰𒄿𒂊𒊒𒄩𒄖';
+    var answer = Translate.Execute(letter);
+    expect(answer, translatedChar);
+  });
+
+  test('If there are only characters that cannot be converted', () {
+    var letter = 'あいうえお123';
+    var translatedChar = 'あいうえお123';
+    var answer = Translate.Execute(letter);
+    expect(answer, translatedChar);
+  });
 }
